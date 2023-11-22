d1e316b4db98809166d0464d4f5012a9d2a7a0a9
lafkpages <luisafk@proton.me>
2023-11-22 18:34:21 +0100

    Show all masks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,22 @@
 	import { qrs, parseQr } from '$lib/qr';
 
 	import Qr from '$components/Qr.svelte';
+	import QrMask from '$components/QrMask.svelte';
 
 	let qr = parseQr(qrs.wikipedia);
 </script>
 
 <Qr {qr} />
+
+<h2>Masks</h2>
+<div class="masks">
+	{#each { length: 8 } as _, mask}
+		<div class="mask">
+			<h4>Mask {mask.toString(2).padStart(3, '0')}</h4>
+			<QrMask {mask} />
+		</div>
+	{/each}
+</div>
 
 <h2>Sources</h2>
 <ul>
@@ -30,3 +41,26 @@
 		</a>
 	</li>
 </ul>
+
+<style lang="scss">
+	.masks {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+
+		.mask {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			h4 {
+				margin-block-start: 0px;
+				margin-block-end: 0px;
+			}
+
+			:global(.data-table) {
+				border: 1px solid black;
+			}
+		}
+	}
+</style>
