e6cd79da4dbc1a4c636884caf75dfa11fe33bfe9
lafkpages <luisafk@proton.me>
2023-11-22 10:39:27 +0100

    QR overlays

--- src/lib/components/Qr.svelte
+++ src/lib/components/Qr.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
 	import { parseQr, type Qr } from '$lib/qr';
+	import { onMount } from 'svelte';
 
 	let rawQr: string | Qr;
 	export { rawQr as qr };
 
 	$: qr = parseQr(rawQr);
+
+	let qrElm: HTMLDivElement;
+
+	let cellSize = 0;
+
+	onMount(() => {
+		cellSize = qrElm.getElementsByTagName('td')[0].clientHeight;
+	});
 </script>
 
-<div class="qr">
+<div class="qr" style:--cell-size="{cellSize}px" bind:this={qrElm}>
 	<table>
 		<tbody>
 			{#each qr as line, lineIndex}
@@ -18,6 +27,8 @@
 				</tr>
 			{/each}
 		</tbody>
+
+		<div class="overlay mask" />
 	</table>
 
 	<div class="info">
@@ -29,19 +40,42 @@
 
 <style lang="scss">
 	div.qr {
+		$cellSize: 8px;
+
 		display: flex;
 		gap: 12px;
 
 		table {
 			border-spacing: 0px;
+			position: relative;
 
-			td.cell {
-				width: 8px;
-				height: 8px;
-				aspect-ratio: 1;
+			tr {
+				height: $cellSize;
 
-				&.on {
-					background-color: black;
+				td.cell {
+					width: $cellSize;
+					height: $cellSize;
+					aspect-ratio: 1;
+
+					&.on {
+						background-color: black;
+					}
+				}
+			}
+
+			div.overlay {
+				position: absolute;
+				background-color: #ff0000aa;
+
+				&:hover {
+					transform: scale(1.2);
+				}
+
+				&.mask {
+					top: calc(8 * var(--cell-size));
+					left: calc(2 * var(--cell-size));
+					width: calc(3 * var(--cell-size));
+					height: calc(var(--cell-size));
 				}
 			}
 		}
