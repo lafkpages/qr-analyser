843a88f57231238764af9875c62b7cae0a25a905
lafkpages <luisafk@proton.me>
2023-11-22 17:54:45 +0100

    More sources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,22 @@
 <h2>Sources</h2>
 <ul>
 	<li>
-		<a href="https://i.stack.imgur.com/N4RFU.png">QR code masks</a>
+		<a href="https://i.stack.imgur.com/N4RFU.png" target="_blank" rel="noopener noreferrer">
+			QR code masks
+		</a>
+	</li>
+	<li>
+		<a
+			href="https://blog.qartis.com/decoding-small-qr-codes-by-hand/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Decoding small QR codes by hand
+		</a>
+	</li>
+	<li>
+		<a href="https://en.wikipedia.org/wiki/QR_code" target="_blank" rel="noopener noreferrer">
+			QR code - Wikipedia
+		</a>
 	</li>
 </ul>
