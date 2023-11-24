380601a1043a76b48152920e1b399b3b2376fac3
lafkpages <luisafk@proton.me>
2023-11-24 12:05:24 +0100

    Even more responsive

--- src/lib/components/QrAnalysis.svelte
+++ src/lib/components/QrAnalysis.svelte
@@ -13,97 +13,123 @@
 </script>
 
 <div class="qr">
-	<Qr {qr} bind:hoveredCellX bind:hoveredCellY />
-
-	<div class="info">
-		<h3>QR info</h3>
-		<p>
-			<span>Size: {qr.size}</span>
-			<br />
-			<span>Version: {qr.version}</span>
-			<br />
-			<span>Raw mask: {qr.rawMaskStr} ({qr.rawMask})</span>
-			<br />
-			<span>Mask: {qr.maskStr} ({qr.mask})</span>
-		</p>
-
-		<QrMask mask={qr.mask} />
+	<div class="left">
+		<Qr {qr} bind:hoveredCellX bind:hoveredCellY />
 	</div>
 
-	<div class="info">
-		<h3>Cell info</h3>
-		<p>
-			{#if hoveredCellX != null && hoveredCellY != null}
-				<span>X: {hoveredCellX}</span>
+	<div class="middle">
+		<div class="info">
+			<h3>QR info</h3>
+			<p>
+				<span>Size: {qr.size}</span>
 				<br />
-				<span>Y: {hoveredCellY}</span>
+				<span>Version: {qr.version}</span>
 				<br />
-				<span>Cell type: {cellTypeLabels[getCellType(qr.size, hoveredCellY, hoveredCellX)]}</span>
-			{:else}
-				Hover over a cell to see its info.
-			{/if}
-		</p>
+				<span>Raw mask: {qr.rawMaskStr} ({qr.rawMask})</span>
+				<br />
+				<span>Mask: {qr.maskStr} ({qr.mask})</span>
+			</p>
+
+			<QrMask mask={qr.mask} />
+		</div>
+
+		<div class="info">
+			<h3>Cell info</h3>
+			<p>
+				{#if hoveredCellX != null && hoveredCellY != null}
+					<span>X: {hoveredCellX}</span>
+					<br />
+					<span>Y: {hoveredCellY}</span>
+					<br />
+					<span>Cell type: {cellTypeLabels[getCellType(qr.size, hoveredCellY, hoveredCellX)]}</span>
+				{:else}
+					Hover over a cell to see its info.
+				{/if}
+			</p>
+		</div>
+
+		<div class="key">
+			<h3>Color key</h3>
+
+			<ul>
+				{#each [['green', cellTypeLabels[CellType.PositionPattern]], ['yellow', cellTypeLabels[CellType.TimingPattern]], ['cyan', 'mask data'], ['orange', 'data encoding'], ['teal', 'inverted cell'], ['magenta', 'hovered cell']] as [color, label]}
+					<li>
+						<div style:--color={color} />
+						{label}
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 
-	<div class="key">
-		<h3>Color key</h3>
-
-		<ul>
-			{#each [['green', cellTypeLabels[CellType.PositionPattern]], ['yellow', cellTypeLabels[CellType.TimingPattern]], ['cyan', 'mask data'], ['orange', 'data encoding'], ['teal', 'inverted cell'], ['magenta', 'hovered cell']] as [color, label]}
-				<li>
-					<div style:--color={color} />
-					{label}
-				</li>
-			{/each}
-		</ul>
+	<div class="right">
+		<Qr {qr} bind:hoveredCellX bind:hoveredCellY unmasked />
 	</div>
-
-	<Qr {qr} bind:hoveredCellX bind:hoveredCellY unmasked />
 </div>
 
 <style lang="scss">
 	@use '$lib/styles/DataTable' as *;
 
 	div.qr {
+		position: relative;
 		display: flex;
 		gap: 12px;
 
-		div.info {
+		.middle {
 			display: flex;
-			flex-direction: column;
+			gap: 12px;
+			flex-grow: 1;
 
-			h3:first-of-type {
-				margin-block-start: 0px;
+			div.info {
+				display: flex;
+				flex-direction: column;
+
+				h3:first-of-type {
+					margin-block-start: 0px;
+				}
+
+				p {
+					margin-block-start: 0px;
+				}
 			}
 
-			p {
-				margin-block-start: 0px;
-			}
-		}
+			div.key {
+				margin-left: auto;
 
-		div.key {
-			margin-left: auto;
+				h3:first-of-type {
+					margin-block-start: 0px;
+				}
 
-			h3:first-of-type {
-				margin-block-start: 0px;
-			}
+				ul {
+					padding: 0px;
 
-			ul {
-				padding: 0px;
+					li {
+						display: flex;
+						align-items: center;
+						gap: 4px;
 
-				li {
-					display: flex;
-					align-items: center;
-					gap: 4px;
+						div {
+							width: $cellSize;
+							height: $cellSize;
+							aspect-ratio: 1;
 
-					div {
-						width: $cellSize;
-						height: $cellSize;
-						aspect-ratio: 1;
-
-						background-color: var(--color);
+							background-color: var(--color);
+						}
 					}
 				}
+			}
+		}
+	}
+
+	// If screen is <1000px wide
+	@media (max-width: 1000px) {
+		div.qr {
+			flex-direction: column;
+
+			.right {
+				position: absolute;
+				top: 0px;
+				right: 0px;
 			}
 		}
 	}
